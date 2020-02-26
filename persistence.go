34bc565030b46d00ebdaad9eb1b84c7bdd3de4de
Benjamin Cooley <benjamincooley81@gmail.com>
2020-02-26 11:24:01 -0800

    Create persistence.go

---
+++ persistence.go
@@ -0,0 +1,40 @@
+package persistence
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/325gerbils/go-files"
+)
+
+var state map[string]string
+var mutex sync.Mutex
+
+// Store stores a string in a persistent key-value map
+func Store(key string, value string) {
+	mutex.Lock()
+	state[key] = value
+	if !files.Exists("data/persistence") {
+		os.Mkdir("data", 0777)
+		jsondata, err := json.Marshal(state)
+		if err != nil {
+			log.Fatal(err)
+		}
+		files.Save("data/persistence", string(jsondata))
+	}
+	mutex.Unlock()
+}
+
+// Get returns a string from a persistent key-value map
+func Get(key string) string {
+	mutex.Lock()
+	if files.Exists("data/persistence") {
+		ps := files.Open("data/persistence")
+		json.Unmarshal([]byte(ps), &state)
+	}
+	v := state[key]
+	mutex.Unlock()
+	return v
+}
